d3720c81dfca7f168cf4c5dd9f8e169b70420ccf
ifrazafind <35964748+ifrazafind@users.noreply.github.com>
2018-12-13 18:24:45 +0300

    Filtre1

--- hw4/src/hw4/Filtre1.java
+++ hw4/src/hw4/Filtre1.java
@@ -2,16 +2,18 @@ package hw4;
 
 public class Filtre1 {
 
+
     public Filtre1 ()
     {
+
 
     }
     public void Filtre (String filtre)
     {
-        if (filtre == "1") {
+        if (filtre.equals("1")) {
             System.out.println("D");
         }
-        else if (filtre == "0")
+        else if (filtre.equals("0"))
             System.out.println("N");
         else {
             System.out.println("Erreur de lecture");
